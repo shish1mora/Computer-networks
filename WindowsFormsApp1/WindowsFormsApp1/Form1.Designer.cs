6851d2a60f30a65d44d4d238cd6c30555155cd84
Siegmeyer0of0Catarina <siegmeyerofcatarina0x@gmail.com>
2024-05-15 17:12:46 +0300

    Update
    lb4

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(995, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(977, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://www.mivlgu.ru/raspisanie?teacher_id=17&semester=2&year=2023#schedule-teac" +
+    "her";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(570, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(500, 200);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 40);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(552, 402);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(570, 242);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(500, 200);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 453);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ЛБ4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> headlines = new List<string>();//Список всех тегов
+        public List<string> links = new List<string>();//Список всех ссылок
+        public List<string> images = new List<string>();//Список всех изображений
+        public List<string> texts = new List<string>();//Список всех текстов
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            WebClient client = new WebClient();
+
+            byte[] bytedata = client.DownloadData(URL);
+            richTextBox1.Text = Encoding.UTF8.GetString(bytedata);
+
+            client.DownloadFile(URL, "Z:\\Index.html");
+
+            webBrowser1.DocumentText = richTextBox1.Text;
+
+            richTextBox2.Text = "";
+            //Список тегов для поиска заголовков
+            string tegsH = @"(<h[1-6].*[A-z].*>|<h[1-6]>)(.*[А-яЁё].*)(<\/h[1-6]>)";
+            Regex regexH = new Regex(tegsH);
+            //Список всех ссылок (название ссылки и сама ссылка)
+            string href = "href=";
+            //Список всех картинок
+            string img = "<img src=";
+            //Чистый неформатированный текст (например, из тегов <p>)
+            string p = "<p>";
+
+            //Создаем список в котороый запишем весь текст построчно
+            List<string> splitLine = new List<string>();
+            //Считываем текст из файла
+            StreamReader myStreamReader = File.OpenText(@"Z:\\Index.html");
+            //Записываем каждую строку текста отдельно
+            while (!myStreamReader.EndOfStream)
+            {
+                splitLine.Add(myStreamReader.ReadLine());
+            }
+          
+            foreach (var line in splitLine)//Проход всех строк текста
+            {                
+                for(int n = 1; n<=6;n++)//Поиск списка тегов заголовков
+                {
+                    if (regexH.IsMatch(line))
+                    {
+                            var rg = new Regex(@">(.*?)<");
+                            var result = rg.Match(line).Groups[1].Value;
+                            headlines.Add($"\tЗаголовок h{n}: {result}\r\n");
+                            richTextBox2.Text += $"Заголовок h{n}: {result}\r\n";
+                    }
+                }
+
+                //Поиск ссылок
+                if (line.Contains(href))
+                {
+                    var rg = new Regex(@">(.*?)<");
+                    var result = rg.Match(line).Groups[1].Value;
+                    var rg1 = new Regex("<a.+?href=[\"'](.+?)[\"'].*?>");
+                    var result1 = rg1.Match(line).Groups[1].Value;
+                    if (result.Length > 1)
+                        links.Add($"\tНазвание ссылки: {result}\r\n\t\t Ссылка: {result1}\r\n");  
+                }
+
+                //Поиск картинок
+                if (line.Contains(img))
+                {
+                    var rg = new Regex("<img.+?src=[\"'](.+?)[\"'].*?>");
+                    var result = rg.Match(line).Groups[1].Value;
+                    var rg1 = new Regex("<img.+?alt=[\"'](.+?)[\"'].*?>");
+                    var result1 = rg1.Match(line).Groups[1].Value;
+                    images.Add($"\tКартинка: {result1}\r\n\t\t Ссылка: {result}\r\n");
+                }
+                //Поиск текста
+                if (line.Contains(p))
+                {
+                    var rg = new Regex(@">(.*?)<");
+                    var result = rg.Match(line).Groups[1].Value;
+                    if (result.Length > 1)
+                        texts.Add($"\tТекст: {result}\r\n");
+                }
+            }
+            ShowAll();
+        }
+
+        private void ShowAll()
+        {
+            richTextBox2.Text = "Список всех тегов:\n";
+            foreach (var h in headlines)
+                richTextBox2.Text += h;
+            richTextBox2.Text += "\nСписок всех ссылок:\n";
+            foreach (var l in links)
+                richTextBox2.Text += l;
+            richTextBox2.Text += "\nСписок всех изображений:\n";
+            foreach (var i in images)
+                richTextBox2.Text += i;
+            richTextBox2.Text += "\nСписок всех текстов:\n";
+            foreach (var t in texts)
+                richTextBox2.Text += t;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
